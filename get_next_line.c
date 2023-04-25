ac2b01b88fdd37d91bd9c1f34b193ddb7c143f9b
edufour/Elodie <lalou_dufour@hotmail.com>
2023-04-25 13:51:16 -0400

    needs optimisation

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: edufour <edufour@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 14:30:41 by edufour           #+#    #+#             */
-/*   Updated: 2023/04/21 15:34:14 by edufour          ###   ########.fr       */
+/*   Updated: 2023/04/25 13:41:15 by edufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_calloc(int nb, int size)
+void	*ft_calloc(int nb, int size)
 {
 	char	*buffer;
 	int		i;
@@ -31,12 +31,12 @@ char	*ft_calloc(int nb, int size)
 	return (buffer);
 }
 
-char	*get_stash(void)
+char	**get_stash(void)
 {
-	static char	*stash;
+	static char	**stash;
 
 	if (stash == NULL)
-		stash = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+		stash = ft_calloc(1, sizeof(char *));
 	return (stash);
 }
 
@@ -63,17 +63,22 @@ char	*make_line(char *base, char *add, int start, int stop)
 	free(base);
 	while (start < stop)
 		new_line[i_new++] = add[start++];
+	if (add)
+		free(add);
 	new_line[i_new] = '\0';
 	return (new_line);
 }
 
-char	*safe_return(char *return_str, char *free_stash, char *free_line)
+char	*safe_return(char *return_str, char **free_stash, char *free_line)
 {
+	if (*free_stash)
+		free (*free_stash);
 	if (free_stash)
 		free (free_stash);
 	if (free_line)
 		free (free_line);
 	free_stash = NULL;
+	*free_stash = NULL;
 	free_line = NULL;
 	return (return_str);
 }
@@ -94,44 +99,51 @@ int	ft_strchr(char *str, char search)
 	return (-1);
 }
 
-int	read_stash(char *stash, char *next_line)
+int	read_stash(char **stash, char *next_line)
 {
 	int	eol;
 	int	end;
 
 	end = 0;
-	while (stash[end])
+	while (*stash[end])
 		end ++;
-	eol = ft_strchr(stash, '\n');
-	next_line = make_line(next_line, stash, 0, eol);
-	//make_line();
-	if (eol == end)
+	eol = ft_strchr(*stash, '\n');
+	if (eol == -1)
+	{	
+		next_line = make_line(next_line, stash, 0, end);
 		return (FLAG_CONTINUE);
+	}
+	next_line = make_line(next_line, stash, 0, eol);
 	return (FLAG_RETURN);
 }
 
-char	*process_read(int fd, char *next_line, char	*stash)
+//calls read until an eol is found or read returns 0. Build next_line and saves, if needed, the unused
+//str in stash.
+char	*process_read(int fd, char *next_line, char	**stash)
 {
-	char	*tmp;
 	int		read_bytes;
+	char	*tmp;
 
-	read_bytes = 1;
-	tmp = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	while (ft_strchr(tmp, '\n' != 1 && read_bytes > 0))
+	while (ft_strchr(next_line, '\n') == 0 && read_bytes > 0)
 	{
+		tmp = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+		if (!tmp)
+			return (safe_return(NULL, next_line, stash));
 		read_bytes = read(fd, tmp, BUFFER_SIZE);
-		if (read_bytes == -1)
-			return (safe_return(NULL, stash, next_line));
-		next_line = make_line(next_line, tmp, 0, read_bytes);
+		if (ft_strchr(tmp, '\n') >= 0)
+			*stash = make_line(NULL, tmp, ft_strchr(tmp, '\n'), BUFFER_SIZE);
+		next_line = make_line(next_line, tmp, 0, ft_strlen(tmp));
 	}
-	if (read_bytes == 0 && tmp[0] == 0);	
+	if (read_bytes == -1 || read_bytes == 0 && next_line[0] == 0)
+		return (safe_return(NULL, next_line, stash));
+	return (next_line);
 }
 
 char	*get_next_line(int fd)
 {
 	int		return_flag;
 	char	*next_line;
-	char	*stash;
+	char	**stash;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: edufour <edufour@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 14:04:55 by edufour           #+#    #+#             */
-/*   Updated: 2023/04/18 15:03:21 by edufour          ###   ########.fr       */
+/*   Updated: 2023/04/25 09:56:54 by edufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
+
+typedef struct strings
+{
+	char	*stash;
+	char	*next_line;
+}		strings;
 
 # define FLAG_ERROR -1
 # define FLAG_CONTINUE 0
