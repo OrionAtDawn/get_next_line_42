ee91c1e95e67b5c985c940cc848d2a58f24d530f
edufour/Elodie <lalou_dufour@hotmail.com>
2023-05-04 15:01:27 -0400

    V.3

--- get_next_line.c
+++ get_next_line.c
@@ -6,132 +6,127 @@
 /*   By: edufour <edufour@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 14:30:41 by edufour           #+#    #+#             */
-/*   Updated: 2023/05/03 11:42:22 by edufour          ###   ########.fr       */
+/*   Updated: 2023/05/04 14:57:49 by edufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-//returns the adress of a pointer to stash. Allocates stash when first called.
-char	**get_stash(void)
-{
-	static char	**stash;
 
-	if (stash == NULL)
+//Calls read until an eol in found or read returns 0/-1.
+//Puts everything in stash.
+char	*read_to_stash(char *stash, int fd)
+{
+	char	*tmp;
+	int		read_bytes;
+
+	read_bytes = 1;
+	while (ft_strchr(stash, '\n') == -1 && read_bytes > 0)
 	{
-		stash = ft_calloc(1, sizeof(char *));
-		*stash = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+		tmp = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+		read_bytes = read(fd, tmp, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			free (tmp);
+			free (stash);
+			stash = NULL;
+			return (NULL);
+		}
+		stash = ft_strjoin(stash, tmp);
+		free (tmp);
 	}
 	return (stash);
 }
 
-/*Allocates sufficient space for a copy of 'add[start]' - 'add[stop]' 
-appended at the end of 'base'. Frees 'base', and returns new_line.*/
-char	*make_line(char *base, char *add, size_t start, size_t stop)
-{
-	char	*new_line;
-	size_t	i_base;
-	size_t	i_add;
-	size_t	i_new;
-
-	i_base = 0;
-	i_add = stop - start;
-	if (!base)
-		base = ft_calloc(1, 1);
-	while (base[i_base])
-		i_base++;
-	new_line = ft_calloc(i_base + i_add + 1, sizeof(char));
-	if (!new_line)
-		return (NULL);
-	i_new = 0;
-	i_base = 0;
-	while (base[i_base])
-		new_line[i_new++] = base[i_base++];
-	free(base);
-	while (start < stop)
-		new_line[i_new++] = add[start++];
-	new_line[i_new] = '\0';
-	return (new_line);
-}
-
-char	*process_stash(char **stash)
+//Returns a copy of everything before the first '\n' (or '\0' if not found)
+char	*stash_to_line(char *stash)
 {
 	int		eol;
+	int		i;
 	char	*next_line;
 
-	eol = ft_strchr(*stash, '\n');
+	if (stash == NULL)
+		return (NULL);
+	eol = ft_strchr(stash, '\n');
+	if (eol == -1)
+		return (ft_strjoin(NULL, stash));
+	next_line = ft_calloc(eol + 2, sizeof(char));
+	i = -1;
+	while (++i <= eol)
+		next_line[i] = stash[i];
+	return (next_line);
+}
+
+//Remakes the stash so that only what's left after the first '\n' remains in it
+//(prepares stash for the next call of get_next_line)
+//Copies the desired portion, frees current stash, 
+//then allocates a new one and calls ft_strjoin(NULL, new_stash).
+char	*keep_in_stash(char *stash)
+{
+	int		eol;
+	int		i;
+	char	*new_stash;
+
+	if (stash == NULL)
+		return (NULL);
+	eol = ft_strchr(stash, '\n');
 	if (eol == -1)
 	{
-		next_line = make_line(NULL, *stash, 0, ft_strlen(*stash));
-		ft_bzero(*stash, ft_strlen(*stash));
+		free (stash);
+		return (NULL);
 	}
-	else
-	{
-		next_line = make_line(NULL, *stash, 0, eol + 1);
-		eol = ft_strchr(*stash, '\n');
-		*stash = make_line(NULL, *stash, eol + 1, ft_strlen(*stash));
-	}
-	return (next_line);
-}
-
-/*Creates make_line from stash.
-Calls read until an eol is found or read returns 0. 
-Build next_line and saves, if needed, the unused str in stash.*/
-char	*process_read(int fd, char	**stash)
-{
-	int		eol;
-	int		read_bytes;
-	char	*tmp;
-	char	*next_line;
-
-	next_line = process_stash(stash);
-	read_bytes = 1;
-	while (ft_strchr(next_line, '\n') == -1 && read_bytes > 0)
-	{
-		tmp = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-		if (!tmp)
-			return (safe_return(NULL, &stash, &next_line));
-		read_bytes = read(fd, tmp, BUFFER_SIZE);
-		eol = ft_strchr(tmp, '\n');
-		if (eol > -1)
-			*stash = make_line(NULL, tmp, eol + 1, ft_strlen(tmp));
-		else
-			eol = ft_strlen(tmp);
-		next_line = make_line(next_line, tmp, 0, eol + 1);
-		free (tmp);
-	}
-	if (read_bytes == -1 || (read_bytes == 0 && next_line[0] == '\0'))
-		return (safe_return(NULL, &stash, &next_line));
-	return (next_line);
+	eol ++;
+	new_stash = ft_calloc((ft_strlen(stash) - eol) + 1, sizeof(char));
+	i = 0;
+	while (stash[eol])
+		new_stash[i++] = stash[eol++];
+	free (stash);
+	return (new_stash);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	**stash;
+	static char	*stash;
+	char		*next_line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	stash = get_stash();
-	if (stash == NULL || *stash == NULL)
+	if (stash == NULL)
+		stash = ft_calloc(BUFFER_SIZE, sizeof(char));
+	if (stash == NULL)
 		return (NULL);
-	return (process_read(fd, stash));
-}
-
-
-#include <fcntl.h>
-#include <stdio.h>
-int main()
-{
-	int	fd;
-	char	*line;
-	fd = open("text.txt", O_RDONLY);
-
-	line = get_next_line(fd);
-	while (line != NULL)
-	{
-		printf("%s", line);
-		free(line);
-		line = get_next_line(fd);
+	stash = read_to_stash(stash, fd);
+	if (stash == NULL)
+		return (NULL);
+	if (stash[0] == 0)
+	{	
+		free(stash);
+		return (NULL);
 	}
-	free (line);
+	next_line = stash_to_line(stash);
+	stash = keep_in_stash(stash);
+	return (next_line);
 }
+
+// #include <fcntl.h>
+// #include <stdio.h>
+// int main()
+// {
+// 	int	fd;
+// 	char	*line;
+// 	fd = open("/Users/edufour/Desktop/42_Projets/get_next_line/42_repo/intra-uuid-57f86113-00e3-4855-b4db-04405e0e327f-4612089-edufour/text.txt", O_RDONLY);
+// 	// printf("%d", fd);
+
+// 	line = get_next_line(fd);
+// 	// printf("%s", line);
+// 	// free(line);
+// 	// line = get_next_line(fd);
+// 	while (line != NULL)
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 		line = get_next_line(fd);
+// 	}
+// 	if (line)
+// 		free (line);
+// }
 
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,11 +6,12 @@
 /*   By: edufour <edufour@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 13:53:01 by edufour           #+#    #+#             */
-/*   Updated: 2023/05/03 11:41:43 by edufour          ###   ########.fr       */
+/*   Updated: 2023/05/04 13:02:22 by edufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include <unistd.h> 
+#include <stdlib.h>
 
 int	ft_strlen(const char *s)
 {
@@ -22,20 +23,6 @@ int	ft_strlen(const char *s)
 	while (s[i])
 		i++;
 	return (i);
-}
-
-void	ft_bzero(void *s, size_t n)
-{
-	size_t	i;
-
-	if (!s)
-		return ;
-	i = 0;
-	while (i < n)
-	{
-		((char *)s)[i] = 0;
-		i++;
-	}
 }
 
 int	ft_strchr(char *str, char search)
@@ -54,30 +41,48 @@ int	ft_strchr(char *str, char search)
 	return (-1);
 }
 
-//frees, if needed, stash and any given line. Returns 'return_str'
-char	*safe_return(char *return_str, char ***free_stash, char **free_line)
-{
-	if (**free_stash)
-		free (**free_stash);
-	**free_stash = NULL;
-	if (*free_stash)
-		free (*free_stash);
-	*free_stash = NULL;
-	if (*free_line)
-		free (*free_line);
-	*free_line = NULL;
-	return (return_str);
-}
-
 void	*ft_calloc(size_t nb, size_t size)
 {
 	char	*buffer;
+	size_t	i;
 
 	if (nb < 0 || size < 0)
 		return (NULL);
 	buffer = malloc (nb * size);
 	if (!buffer)
 		return (NULL);
-	ft_bzero(buffer, nb * size);
+	i = 0;
+	while (i < nb * size)
+	{
+		buffer[i] = 0;
+		i++;
+	}
 	return (buffer);
+}
+
+//Allocates enough memory to append 'add' to 'base', does the operation, 
+//frees 'base' and returns the new allocated char *.
+char	*ft_strjoin(char *base, char *add)
+{
+	char		*new_line;
+	int			i_base;
+	int			i_add;
+
+	i_base = -1;
+	i_add = -1;
+	if (!base)
+		base = calloc(1, 1);
+	if (!add)
+		return (NULL);
+	new_line = malloc(ft_strlen(base) + ft_strlen(add) + 1);
+	if (!new_line)
+		return (NULL);
+	while (base[++i_base])
+		new_line[i_base] = base[i_base];
+	free (base);
+	base = NULL;
+	while (add[++i_add])
+		new_line[i_base + i_add] = add[i_add];
+	new_line[i_base + i_add] = '\0';
+	return (new_line);
 }
